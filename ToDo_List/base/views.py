bf7fd696bea8432a8e0708bc15623ec427d383bd
Milosz Bochenek <miloszbochenek20@gmail.com>
2022-11-19 17:57:01 +0100

    Adding login/out functionality

--- ToDo_List/base/views.py
+++ ToDo_List/base/views.py
@@ -4,9 +4,22 @@ from django.views.generic.detail import DetailView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
+
 from .models import Task
 
 # Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
 class TaskList(ListView):
     model = Task
     context_object_name  = 'tasks'
@@ -35,4 +48,3 @@ class DeleteView(DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
-
