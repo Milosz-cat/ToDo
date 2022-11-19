4b1a2df4eb6c80ef10b04703c804d9edd77161fa
Milosz Bochenek <miloszbochenek20@gmail.com>
2022-11-19 22:11:37 +0100

    Repairing project to work properly

--- ToDo_List/base/views.py
+++ ToDo_List/base/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
@@ -10,13 +9,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
-# Imports for Reordering Feature
-from django.views import View
-from django.shortcuts import redirect
-from django.db import transaction
-
 from .models import Task
-from .forms import PositionForm
+
 
 # Create your views here.
 class CustomLoginView(LoginView):
@@ -40,10 +34,10 @@ class RegisterPage(FormView):
             login(self.request, user)
         return super(RegisterPage, self).form_valid(form)
     
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated():
-            return redirect('tasks')
-        return super(RegisterPage, self).get(*args, **kwargs)
+    # def get(self, *args, **kwargs):
+    #     if self.request.user.is_authenticated():
+    #         return redirect('tasks')
+    #     return super(RegisterPage, self).get(*args, **kwargs)
     
 
 class TaskList(LoginRequiredMixin, ListView):
@@ -91,14 +85,4 @@ class DeleteView(LoginRequiredMixin, DeleteView):
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
 
-class TaskReorder(View):
-    def post(self, request):
-        form = PositionForm(request.POST)
 
-        if form.is_valid():
-            positionList = form.cleaned_data["position"].split(',')
-
-            with transaction.atomic():
-                self.request.user.set_task_order(positionList)
-
-        return redirect(reverse_lazy('tasks'))
